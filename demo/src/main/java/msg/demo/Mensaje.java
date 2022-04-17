0042796ee40f8bd9e426cc4a687c26976628306b
dinero450 <ramon.parra@usach.cl>
2022-04-17 17:07:57 -0400

    App mingeso

---
+++ demo/src/main/java/msg/demo/Mensaje.java
@@ -0,0 +1,16 @@
+package msg.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Mensaje {
+    
+    @GetMapping
+    public String mensajeApp()
+    {
+        
+        return "Aplicacion web muy sencilla";
+    }
+}
